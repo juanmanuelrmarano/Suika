5db54404753c5dd89575dca8538fae7db216eea9
juanmanuelrmarano <juanmanuelrmarano@gmail.com>
2021-09-28 04:56:59 -0300

    Update settings.py

--- suika/settings.py
+++ suika/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '546-hz%j$x0u7z5sp$eri04)jtsji_$%z(0fv603722r!6(vs7'
+# SECRET_KEY = '546-hz%j$x0u7z5sp$eri04)jtsji_$%z(0fv603722r!6(vs7'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
