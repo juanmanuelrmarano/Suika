0a1fe43336a6f48eadda154c3ff0f0d205166d1a
juanmanuelrmarano <juanmanuelrmarano@gmail.com>
2021-09-28 05:37:00 -0300

    Update settings.py

--- suika/settings.py
+++ suika/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['suikaapp.herokuapp.com','127.0.0.1']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
